05e7276650fa4f9a5a5b7fb1fe1e7fbfb3a30413
swati <91478967+swati-kaithwas@users.noreply.github.com>
2021-11-08 16:59:06 +0530

    gdf

--- coding/foodapp/components/Showdata.js
+++ coding/foodapp/components/Showdata.js
@@ -4,4 +4,18 @@ async function getData(url)
     let data=await res.json()
 
     return data
+    function append(data,cont,count2){
+        let{strMeal,strMealThumb,}
+
+        let div =document.createElement("div");
+        let img=document.createElement("img");
+        img.src=strMealThumb;
+        let p=document.createElement("h2");
+        p.innerText=strMeal;
+        div.append(p);
+        cont.append(div.img)
+        // count2.append()
+
+    }
 }
+export { getData ,append }
