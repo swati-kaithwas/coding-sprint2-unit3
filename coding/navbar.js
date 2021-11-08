9a41e597ab530215b7ec33cd5a398933ddcf8164
swati <91478967+swati-kaithwas@users.noreply.github.com>
2021-11-08 15:25:27 +0530

    Update navbar.js

---
+++ coding/navbar.js
@@ -0,0 +1,18 @@
+function navbar()
+{
+    return `
+    
+    <h3><a href="/"> Home</h3>
+    <div>
+   <inputid="search" type="text" placeholder="search"/>
+  <button id="btn">Search<button>
+  </div>
+  <div id="sequence">
+  
+  
+    <h3><a href=""></a></h3>
+    <h3><a href=""></a></h3>
+    <h3><a href=""></a></h3>
+    <h3><a href=""></a></h3>
+ </div>  `
+}
